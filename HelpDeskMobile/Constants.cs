aa94abb15943097df6e57afecb9193bf7167de07
fcartu <fcartu@gmail.com>
2016-11-22 06:24:09 -0500

    Initial commit

---
+++ HelpDeskMobile/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HelpDeskMobile
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://helpdeskmobile.azurewebsites.net";
+	}
+}
+
